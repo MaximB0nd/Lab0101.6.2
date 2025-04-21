d11a647fa202f7cf940f89481fb8c7acc7f8f9c7
MaximB0nd <sersim11@yandex.ru>
2025-04-21 12:16:49 +0300

    Init

---
+++ Lab0101.5.2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Lab0101._5._2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Text file was opened");
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Clear();
+            TextBox2.Clear();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
